d95459b67103af57a383894f0dcfba8a05168470
LFZDavid <lemaagicien@gmail.com>
2021-02-20 13:25:37 +0100

    add Trick:fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Trick;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for($i = 1; $i<20;$i++) {
+            $trick = new Trick();
+            $trick
+            ->setName('Trick n° '.$i)
+            ->setDescription('Description du Trick n° '.$i)
+            ->setCreatedAt(new DateTime());
+            $manager->persist($trick);
+        }
 
         $manager->flush();
     }
