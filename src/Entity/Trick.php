45d103d18a48de4f9b08626da115a1b44f266944
LFZDavid <lemaagicien@gmail.com>
2021-02-26 15:04:05 +0100

    add validation

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -2,11 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\TrickRepository;
 use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=TrickRepository::class)
@@ -22,6 +22,12 @@ class Trick
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *  min = 3, 
+     *  max = 50, 
+     *  minMessage = "Trop court! min :{{ limit }}",
+     *  maxMessage = "Trop long! max :{{ limit }}",
+     * )
      */
     private $name;
 
